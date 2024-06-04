aa7de01dd00b7872e11b5dc05c17e87dce3acfdc
JPV90s <133875714+JPV90s@users.noreply.github.com>
2024-06-04 19:16:02 -0400

    Update filterToDos function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const addToDo = document.querySelector("#add-todo");
+const showAll = document.getElementById("show-all");
 const toDoForm = document.querySelector("#todo-form");
 const titleInput = document.querySelector("#title-input");
 const priorityInput = document.querySelector("#priority-input");
@@ -9,16 +10,29 @@ const dialogToDo = document.querySelector(".dialog-todo");
 const submitOrEditToDo = document.querySelector("#submit-todo");
 const cancelBtn = document.querySelector("#cancel-btn");
 const todoList = document.querySelector(".todo-list");
+const filterText = document.querySelector(".filter-text");
 
 const toDoData = JSON.parse(localStorage.getItem("data")) || [];
 let currentToDo = {};
 
-addToDo.addEventListener("click", () => dialogToDo.showModal());
-cancelBtn.addEventListener("click", () => dialogToDo.close());
-submitOrEditToDo.addEventListener("click", () => dialogToDo.close());
+addToDo.addEventListener("click", () => {
+  submitOrEditToDo.value = "Add Task";
+  dialogToDo.showModal();
+});
+cancelBtn.addEventListener("click", () => {
+  dialogToDo.close();
+  toDoForm.reset();
+});
+submitOrEditToDo.addEventListener("click", () => {
+  dialogToDo.close();
+});
+
+showAll.addEventListener("click", () => {
+  updateToDoContainer();
+  filterText.innerText = "ALL";
+});
 
 const addOrUpdateToDo = () => {
-  submitOrEditToDo.innerText = "Add Todo";
   const dataArrIndex = toDoData.findIndex((item) => item.id === currentToDo.id);
   const taskObj = {
     id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
@@ -37,7 +51,6 @@ const addOrUpdateToDo = () => {
 
   localStorage.setItem("data", JSON.stringify(toDoData));
   updateToDoContainer();
-  reset();
 };
 
 const updateToDoContainer = () => {
@@ -45,7 +58,7 @@ const updateToDoContainer = () => {
 
   toDoData.forEach(({ id, title, priority, category, description, date }) => {
     todoList.innerHTML += `
-    <div class="${id} todo">
+    <div class="${id} todo" id="${id}">
     <div class="left-todo">
       <input type="checkbox" />
     </div>
@@ -58,24 +71,22 @@ const updateToDoContainer = () => {
         <p><strong>Priority: </strong><i> ${priority}</i></p>
       </div>
     </div>
-    <div class="right-todo" id="${id}">
-      <button
-        onclick="editToDo(this)"
-        type="button"
-        class="btn"
-        id="edit-btn"
+    <button
+      onclick="editToDo(this)"
+      type="button"
+      class="btn"
+      id="edit-btn"
       >
         Edit
-      </button>
-      <button
-        onclick="deleteToDo(this)"
-        type="button"
-        class="btn"
-        id="delete-btn"
+    </button>
+    <button
+      onclick="deleteToDo(this)"
+      type="button"
+      class="btn"
+      id="delete-btn"
       >
         Delete
-      </button>
-    </div>
+    </button>
   </div>
         `;
   });
@@ -87,7 +98,7 @@ const deleteToDo = (buttonEl) => {
   );
 
   const toDoIdDelete = toDoData[dataArrIndex].id;
-  const toDoElementDelete = document.querySelector(`.${toDoIdDelete}`);
+  const toDoElementDelete = document.getElementById(`${toDoIdDelete}`);
 
   toDoElementDelete.remove();
 
@@ -108,17 +119,8 @@ const editToDo = (buttonEl) => {
   descriptionInput.value = currentToDo.description;
   dateInput.value = currentToDo.date;
 
-  submitOrEditToDo.innerText = "Update Task";
   dialogToDo.showModal();
-};
-
-const reset = () => {
-  titleInput.value = "";
-  priorityInput.value = "";
-  categoryInput.value = "";
-  descriptionInput.value = "";
-  dateInput.value = "";
-  currentTask = {};
+  submitOrEditToDo.value = "Update Task";
 };
 
 if (toDoData.length) {
@@ -126,41 +128,64 @@ if (toDoData.length) {
 }
 
 toDoForm.addEventListener("submit", (e) => {
-  e.preventDefault();
   addOrUpdateToDo();
 });
 
-// Function to filter to-dos by category
-const filterToDosByCategory = (category) => {
-  // Clear the current to-do list display
+const filterToDos = (filterOption) => {
   todoList.innerHTML = "";
 
-  // Filter the toDoData array by the given category
-  const filteredToDos = toDoData.filter((todo) => todo.category === category);
-
-  // Iterate over the filtered to-dos and add them to the display
-  filteredToDos.forEach(
-    ({ id, title, priority, category, description, date }) => {
-      todoList.innerHTML += `
-      <div class="todo ${id}">
-        <div class="left-todo">
-          <input type="checkbox" />
-        </div>
-        <div class="middle-todo">
-          <div class="top-middle">
-            <p><strong>Title: </strong> <i> ${title}</i></p>
-          </div>
-          <div class="bottom-middle">
-            <p><strong>Due date: </strong> <i> ${date}</i></p>
-            <p><strong>Priority: </strong><i> ${priority}</i></p>
-          </div>
-        </div>
-        <div class="right-todo" id="${id}">
-          <button onclick="editToDo(this)" type="button" class="btn" id="edit-btn">Edit</button>
-          <button onclick="deleteToDo(this)" type="button" class="btn" id="delete-btn">Delete</button>
-        </div>
-      </div>
-    `;
-    }
+  const filteredToDos = toDoData.filter(
+    (todo) => todo.category === filterOption || todo.priority === filterOption
   );
+
+  filterText.innerText = filterOption.toUpperCase();
+
+  if (filteredToDos.length) {
+    filteredToDos.forEach(
+      ({ id, title, priority, category, description, date }) => {
+        todoList.innerHTML += `
+        <div class="todo ${id}">
+          <div class="left-todo">
+            <input type="checkbox" />
+          </div>
+          <div class="middle-todo">
+            <div class="top-middle">
+              <p><strong>Title: </strong> <i> ${title}</i></p>
+            </div>
+            <div class="bottom-middle">
+              <p><strong>Due date: </strong> <i> ${date}</i></p>
+              <p><strong>Priority: </strong><i> ${priority}</i></p>
+            </div>
+          </div>
+            <button onclick="editToDo(this)" type="button" class="btn" id="edit-btn">Edit</button>
+            <button onclick="deleteToDo(this)" type="button" class="btn" id="delete-btn">Delete</button>
+        </div>
+      `;
+      }
+    );
+  } else {
+    todoList.innerHTML += `<p id="no-match"> There are no ToDos that match the current selection.</p>`;
+  }
 };
+
+const categoryContainer = document.querySelector(".categories");
+
+categoryContainer.addEventListener("click", (event) => {
+  const clickedCategory = event.target.closest(".category");
+  if (clickedCategory) {
+    const categoryId = clickedCategory.id;
+    console.log(categoryId);
+    filterToDos(categoryId);
+  }
+});
+
+const priorityContainer = document.querySelector(".priorities");
+
+priorityContainer.addEventListener("click", (event) => {
+  const clickedPriority = event.target.closest(".priority");
+  if (clickedPriority) {
+    const priorityId = clickedPriority.id;
+    console.log(priorityId);
+    filterToDos(priorityId);
+  }
+});
